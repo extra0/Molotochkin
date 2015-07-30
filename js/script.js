92652e5590fbcb2ae531d2c0fa73365e95f9ee14
Ростислав <extra0@mail.ru>
2015-07-31 00:11:12 +0300

    готова главная

--- js/script.js
+++ js/script.js
@@ -1,5 +1,15 @@
 $(function(){
+	// вызов слайдера
+	$('.slider__list').bxSlider({
+		pager: false,
+		moveSlides: 1,
+		minSlides: 6,
+		maxSlides: 6,
+		slideWidth: 177,
+		slideMargin: 30
+	});
 
-	//
+	// модальные окна
+	$('.fancy').fancybox();
 
 });
