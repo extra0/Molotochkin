3b5d75321eeea369b4ea5807f37be7c362badc27
Ростислав <extra0@mail.ru>
2015-07-31 10:47:36 +0300

    редактирование карты на главной

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-$(function(){
+$(function() {
 	// вызов слайдера
 	$('.slider__list').bxSlider({
 		pager: false,
@@ -11,5 +11,32 @@ $(function(){
 
 	// модальные окна
 	$('.fancy').fancybox();
+
+	// яндекс карта 
+	ymaps.ready(function() {
+		var myMap = new ymaps.Map('map', {
+				center: [55.751574, 37.573856],
+				zoom: 13,
+				controls: ['zoomControl']
+			}, {
+				searchControlProvider: 'yandex#search'
+			}),
+			myPlacemark = new ymaps.Placemark(myMap.getCenter(), {
+				hintContent: 'Собственный значок метки'
+			}, {
+				// Опции.
+				// Необходимо указать данный тип макета.
+				iconLayout: 'default#image',
+				// Своё изображение иконки метки.
+				iconImageHref: 'img/marker.png',
+				// Размеры метки.
+				iconImageSize: [29, 33],
+				// Смещение левого верхнего угла иконки относительно
+				// её "ножки" (точки привязки).
+				iconImageOffset: [-3, -33]
+			});
+
+		myMap.geoObjects.add(myPlacemark);
+	});
 
 });
